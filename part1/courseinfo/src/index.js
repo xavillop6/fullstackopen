aad3534c12e1eef6ee4c5a0cae9053937d5c88d0
Xavi <xavillop6@gmail.com>
2022-04-01 14:11:18 +0200

    Part 1 fixes

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -20,7 +20,7 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  return <p>Number of exercises {props.parts[0].exercises + props.parts[0].exercises + props.parts[0].exercises}</p>
+  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
 }
 
 const App = () => {
