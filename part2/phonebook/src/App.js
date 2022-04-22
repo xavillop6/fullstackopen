7b65571802f11c337e1bf2d3cbd0f7f855036f93
Xavi <xavillop6@gmail.com>
2022-04-22 20:00:42 +0200

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Persons = ({ personsToShow }) =>
   <div>
@@ -27,17 +28,19 @@ const Filter = ({ value, onChange }) =>
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
-  const [personsToShow, setPersonsToShow] = useState(persons)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
   
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -50,11 +53,6 @@ const App = () => {
   const handleFilterChange = (event) => {
     const newFilter = event.target.value;
     setFilter(newFilter);
-    const regex = new RegExp(newFilter, 'i');
-    const filteredPersons = persons.filter((person) => {
-      return person.name.match(regex);
-    })
-    setPersonsToShow(filteredPersons);
   }
 
 
@@ -65,13 +63,19 @@ const App = () => {
     } else {
       const personObject = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length + 1
       };
       setPersons(persons.concat(personObject));
       setNewName("");
       setNewNumber("")
     }
   }
+
+  const regex = new RegExp(filter, 'i');
+  const personsToShow = persons.filter((person) => {
+    return person.name.match(regex);
+  })
 
   return (
     <div>
