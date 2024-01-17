d01818eb4ad3cd683a262e800dccb6e554bbfb3d
Xavier Llop <xavi.llop@epigeon.net>
2024-01-17 17:29:15 +0100

    4.10: Pruebas de lista de blogs, paso 3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -39,6 +39,31 @@ describe('testing GET requests', () => {
   })
 })
 
+describe('testing POST requests', () => {
+  // escribe un test en jest que compruebe la llamada POST de express de un blog nuevo y revisa que el numero total de blogs a incrementado a uno respecto a initialBlogs
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'My blog',
+      author: 'Xavier Llop',
+      url: 'https://myblog.com',
+      likes: 7
+    }
+
+    await api.post('/api/blogs/')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const contents = blogsAtEnd.map(blog => blog.title)
+    expect(contents).toContain(
+      newBlog.title
+    )
+  })
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
