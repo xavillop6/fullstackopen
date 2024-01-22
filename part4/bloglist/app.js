9d501254c39838dce9eddb53a2879c56753d136e
Xavier Llop <xavi.llop@epigeon.net>
2024-01-22 17:22:50 +0100

    4.19: expansión de la lista de blogs , paso 7

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -8,6 +8,7 @@ require('express-async-errors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middlewares')
 mongoose.set('strictQuery', false)
 
@@ -25,6 +26,7 @@ app.use(express.json())
 
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/Blog')
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -10,21 +11,31 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
   if (body.title === undefined || body.url === undefined) {
     response.status(400).end()
   } else {
-    const users = await User.find({})
-    const user = users[0]
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes ? body.likes : 0,
-      user: user._id
+      user: user.id
     })
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     user.save()
+
+    delete savedBlog.user
 
     response.status(201).json(savedBlog)
   }
@@ -48,5 +59,13 @@ blogsRouter.put('/:id', async (request, response) => {
   await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
   response.json(blog)
 })
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 module.exports = blogsRouter
