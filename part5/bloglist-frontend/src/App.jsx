ddc380de0a6de4b4bba5bd4041890a1ffc7de7bf
Xavier Llop <xavi.llop@epigeon.net>
2024-01-24 17:54:35 +0100

    5.3: frontend de la lista de blogs, paso 3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import AddBlogForm from './components/AddBlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -80,10 +81,18 @@ const App = () => {
     )
   }
 
+  const createBlog = (newBlog) => {
+    blogService.create(newBlog).then(returnedBlog => {
+      const newBlogs = [...blogs, returnedBlog]
+      setBlogs(newBlogs)
+    })
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      <div>{user.name} logged in <button onClick={handleLogout}>logout</button></div>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+      <AddBlogForm createBlog={createBlog} />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
