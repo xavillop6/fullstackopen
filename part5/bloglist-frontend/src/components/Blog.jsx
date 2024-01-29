884498ea44428736ea6c1ae168ec9bb556f127e7
Xavier Llop <xavi.llop@epigeon.net>
2024-01-29 11:59:27 +0100

    5.7* Frontend de la lista de blogs, paso 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showDetails, setShowDetails] = useState(false)
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const showWhenVisible = { display: showDetails ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleShowDetails}>{showDetails ? 'hide' : 'view'}</button>
+      <div style={showWhenVisible}>{showDetails ? blog.url : null}</div>
+      <div style={showWhenVisible}>likes: {blog.likes} <button>likes</button></div>
+      <div style={showWhenVisible}>{blog.user !== null && blog.user.name}</div>
+    </div>
+  )
+}
 
 export default Blog
